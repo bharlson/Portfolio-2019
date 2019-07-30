c422bbc98efe98616816c70ca60c9e7caac04ad2
bharlson <bjorn.harlson@gmail.com>
2019-07-29 18:21:07 -0700

    first commit

---
+++ components/Logo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="logo">
+  </div>
+</template>
+
+<style lang='scss'>
+  .logo{
+    width:100px;
+    height: 100px;
+    background-size:cover;
+    background-position:center;
+    background-image:url(../assets/img/BHlogo-01.svg);
+    &:hover,&:active{
+      background-image:url(../assets/img/BHlogo-02.svg);
+      cursor:pointer;
+    }
+  }
+</style>
