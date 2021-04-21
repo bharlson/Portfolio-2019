3d4e4362c2c3f51e668efe5fdde76ffb72cddd9c
bharlson <bjorn.harlson@gmail.com>
2021-04-20 19:19:37 -0700

    Added storyblok explanation

--- pages/nikkita4nine/index.vue
+++ pages/nikkita4nine/index.vue
@@ -36,10 +36,27 @@
             </div>
             <div class="row">
                 <div class="col">
-                    <p>In early 2021, an old coworker from the Shaun Scott campaign texted me about doing another website for a new candidate. I said yes on a whim as I was due for a new freelance project. I was excited to find out the "new candidate" was actually a local hero of mine: activist, lawyer, poet and former mayoral candidate Nikkita K.O. Oliver.</p>
-                    <p>
-                        With the time crunch before the campaign launch, I started the site as a static nuxt.js project the same way as I had with Shaun Scott's site. With the help of my friend and co-developer Tariku Allen
-                    </p>
+                    <p>In early 2021, an old coworker from <nuxt-link to="/scott2019">the Shaun Scott campaign</nuxt-link> texted me about doing another website for a new candidate. I said yes on a whim as I was due for a new freelance project. I was excited to find out the "new candidate" was actually a local hero of mine: activist, lawyer, poet and former mayoral candidate Nikkita K.O. Oliver.</p>
+                    <p>I pulled two late night coding sessions to get the initial site live in time for   the site as a static nuxt.js project the same way as I had with Shaun Scott's site.</p>
+                </div>
+            </div>
+        </section>
+        <section id="storyblok">
+            <div class="row">
+                <div class="col">
+                    <h2>Implementing Storyblok</h2>
+                    <p>After the launch, we brought on my friend Tariku Allen as a co-developer. Tariku made the wise suggestion that rather than keeping the site static we should implement a headless CMS so the campaign staff wouldn't have to come through us for every minor change.</p>
+                    <p>We opted for <a href="https://storyblok.com" target="_blank">storyblok</a>, a headless CMS that works well with the nuxt.js and began the process of rebuilding to allow editing in the storyblok interface.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-5">
+                    <project-img-scrollable name="raw-code" src="/nikkita4nine/ourpolicies-raw-code.png" caption="Homepage: Policies section hardcoded"/>
+                    <p>This works...</p>
+                </div>
+                <div class="col-lg-7 mt-4 mt-md-0">
+                    <project-img preview="true" src="/nikkita4nine/ourpolicies-storyblok.jpg" caption="Homepage: Policies section in storyblok"/>
+                    <p>But this is much easier to understand for a lay person.</p>
                 </div>
             </div>
         </section>
@@ -105,6 +122,18 @@ export default {
             height:627px;
         }
     }
+}
+.raw-code{
+    height:402px;
+    // @media (min-width: $md){
+    //     height:480px;
+    // }
+    // @media (min-width: $lg){
+    //     height:500px;
+    // }
+    // @media (min-width: $xl){
+    //     // height:627px;
+    // }  
 }
 </style>
 
