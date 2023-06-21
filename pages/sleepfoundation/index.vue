96dad9398f9b59d423451776458dea05e0f0c2c4
bharlson <bjorn.harlson@gmail.com>
2023-06-21 16:34:51 -0700

    images for sleepfoundation page

--- pages/sleepfoundation/index.vue
+++ pages/sleepfoundation/index.vue
@@ -51,10 +51,12 @@
             </div>
         </section>
         <section id="redesign">
-            <div class="row">
+            <div class="row mt-4">
                 <div class="col">
                     <h2>Redesign</h2>
-                    <p>During 2021 and 2022, Onecare Media grew rapidly as an organization and I was tasked along with my partners on the engineering team with revamping the Wordpress themes used across our suite of web properties, starting with Sleep Foundation.</p>
+                    <p>During 2021 and 2022, Onecare Media grew rapidly as an organization, and added multiple new brands to our portfolio. With this growth, it was necessary to set consistent standards for the design and structure of our websites and online presence.</p>
+                    <p>As a member of the engineering team, I was first tasked with working on the redesign of the wordpress theme used on our #1 site: Sleepfoundation.org.</p>
+                    <p>On the design side, we were making a radical shift towards a darker color scheme, so an interim theme was developed as a midway point.</p>
                 </div>
             </div>
             <div class="row">
@@ -77,20 +79,90 @@
                     </project-img-scrollable>
                </div>
             </div>
+        </section>
+        <section id="tech-stack">
+            <div class="row mt-4">
+                <div class="col">
+                    <h2>Tech Stack</h2>
+                    <p>We started with the structural changes first, implementing a new tech stack optimized for clean code, a modular component structure and logical, replicable CSS standards building off Bootstrap.</p>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col">
+                    <h3>Gutenberg Blocks</h3>
+                    <p>In 2018 Wordpress added Gutenberg Blocks which allow for the creation of reusable modular UI components, in a way that JAMstack libraries like React, Angular and Vue operate. </p>
+                    <p>With the rest of our tech stack we created a library of blocks that could be used by our content editors to intuitively structure the site using the WYSIWYG editor without having to write a line of code.</p>
+                    <p>(Example images from <a href="https://www.advancedcustomfields.com/blog/acf-5-8-introducing-acf-blocks-for-gutenberg/" target="_blank">The Advanced Custom Fields Blog</a>)</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <project-img src="sleepfoundation/gutenberg-blocks.png" preview="true" caption="Selecting a block in the WYSIWYG Editor" />
+                </div>
+                <div class="col-md-6">
+                    <project-img src="sleepfoundation/acf-fields-editor-example.jpg" preview="true" caption="Editing a block" />
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col">
+                    <h3>Advanced Custom Fields</h3>
+                    <p>A popular wordpress plugin for creating new content fields outside those included in the WP core. We used ACF to scaffold the backend content fields needed by our editors and writers to keep the site up to date in the Gutenberg WYSIWYG editor.</p>
+                    <project-img src="sleepfoundation/acf-blocks-field-group.png" preview="true" caption="" />
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-md-7">
+                    <h3>Twig by Symphony</h3>
+                    <p>A templating engine for PHP allowing for semantic code and easily extendable templates. We use twig extensively for markup and logic across the theme. </p>
+                    <p>Loops and conditionals are expressed by wrapping markup in {% for %} and {% if %} tags respectively, and dynamic values (in this case coming from ACF fields) are passed via double curly braces, as shown in the example image.</p>
+                </div>
+                <div class="col">
+                    <project-img src="sleepfoundation/twig-example.png" caption="Example of Twig markup" />
+                </div>
+            </div>
 
         </section>
-        <section id="reflection">
+        <section id="contributions">
+            <div class="row mt-4">
+                <div class="col">
+                    <h2>My contributions</h2>
+                </div>
+            </div>
+
+            <!-- <div class="row">
+                <div class="col">
+                    <h3>Homepage</h3>
+                </div>
+            </div> -->
             <div class="row">
                 <div class="col">
-                    <h2>Reflection</h2>
+                    <project-img src="sleepfoundation/desktop-nav.jpg" caption="Main Navigation: Desktop" />
                 </div>
             </div>
             <div class="row">
+                <div class="col-md-4">
+                    <project-img src="sleepfoundation/mobile-nav.jpg" caption="Main Navigation: Mobile" />
+                </div>
                 <div class="col">
-                    <p>
-                    </p>
+                    <project-img src="sleepfoundation/homepage-banner.png" caption="Homepage Banner Block" />
                 </div>
             </div>
+            <!-- <div class="row">
+                <div class="col">
+                    <project-img src="sleepfoundation/homepage-banner.png" caption="Homepage Banner Block" />
+                </div>
+            </div> -->
+            <div class="row">
+                <div class="col-md-8">
+                    <project-img src="sleepfoundation/sleep-habits-banner.jpg" caption="Sleep Habits Banner Block, used on the homepage" />
+                </div>
+                <div class="col">
+                    <project-img src="sleepfoundation/author-block.png" caption="Author Block, used on <a href='https://www.sleepfoundation.org/about-us/who-we-are' target='_blank'>'Who We Are'</a> " />
+                </div>
+            </div>
+
+
+
         </section>
         <section id="credit">
             <div class="row mt-4">
